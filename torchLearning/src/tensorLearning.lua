fdafe39905f1c5fa25627dec6830b9a00dc323ad
fangzai <wanghaitao8118@163.com>
2015-11-26 16:02:24 +0800

    tensor related

--- torchLearning/src/tensorLearning.lua
+++ torchLearning/src/tensorLearning.lua
@@ -1,4 +1,5 @@
 require "torch"
+require "math"
 
 -- 关于torch中 tensor的处理
 -- 构造函数 torch.Tensor(4,6,6,5)相当于构造一个4D的向量 维度分别是给定的参数
@@ -19,3 +20,93 @@ for i = 1, s:size() do  --继续强调，下标是从1开始的
         s[i] = i
 end
 print(x)
+-- 使用匿名函数
+i = 0
+x:zero()  -- 所有的元素置为0
+print(x)
+x:apply(function()
+        i = i +1
+        return i
+end
+)
+print(x)
+
+print(x:stride())   --表示从第二维开始，他们下标的跨越值
+-- 在lua里面x是userdata类型，但是在torch里面x的类型是torch.DoubleTensor
+print(type(x), "\n",torch.type(x))
+-- 所以对应而言还有ByteTensor, CharTensor, ShortTensor, IntTensor以及FloadTensor
+-- 如果需要修改默认的生成的Tensor类型，可以用 torch.setdefaulttensortype('torch.FloatTensor')
+
+x = torch.Tensor(5):zero()  -- zero() 元素全部置0
+print(x)
+x = torch.Tensor(1, 5):zero()
+print(x)
+
+y = torch.Tensor(x:size()):copy(x)
+-- 等价于 y = x:clone()
+print(y)
+
+x = torch.Tensor(2,5):fill(math.pi)
+print(x)
+y = torch.Tensor(x)  --这句话只是将x的句柄赋给y了，如果改变了x的元素，y的元素也会改变的
+print(y)  
+x[2][3] = 0
+print(y)   --  这个例子的结论是，一般不会使用这种构造函数
+
+-- 还有一种构造函数，就是torch.Tensor(torch.LongStorage({2, 4, 4}))
+x = torch.Tensor(torch.LongStorage({2, 4, 4}))
+x:fill(1)
+print("x 的维度是 ： ", x:nDimension())
+print("x每个维度上的size是：" )
+print(x:size())
+
+
+x = torch.Tensor(torch.LongStorage({4}), torch.LongStorage({0})):zero()
+-- 这个地方好奇怪，明明最后一维是0了，但是还是输出了一个结果
+-- 和 torch.Tensor(torch.LongStorage({4}), torch.LongStorage({0})):zero() 似乎是等价的
+print(x:size())
+
+-- 另一种构造函数，涉及到memcpy
+s = torch.Storage(10):fill(2)
+print(s)  -- 得到的其实是一个一维向量
+-- 如果想将这个向量转化一下
+x = torch.Tensor(s, 1, torch.LongStorage({2, 5}))
+print(x)  --这里的第二个参数相当于s中的起始偏量
+
+-- 但是这里的构造和上面的也是类似的，只是一个浅层拷贝，把句柄赋值过去了而已
+x:zero()
+print(s)  -- s的所有元素也都变成了0
+
+-- 最原始的构造函数  torch.Tensor(table),这里用到了lua里面的table这种数据结构
+x = torch.Tensor({{1, 2, 3, 4}, {5, 6, 7, 8}})
+print("x的内容是 ：")
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ torchLearning/src/tensorManipulating.lua
@@ -0,0 +1,90 @@
+require "torch"
+require "math"
+
+-- 关于tensor的操作函数
+--[[
+-- clone() 函数， 该函数拷贝内存，而不仅仅是句柄
+--]]
+i = 0
+x = torch.Tensor(5,1):apply(
+function(x)
+        i = i +1
+        return i
+end
+)
+y = x:clone()  -- 从下面的输出可以看到改变y之后，对x是不影响的，反之也是
+print(y:t())
+print(torch.Tensor(5):size())
+y:fill(1)
+print(x:t())
+print(y:t())
+
+--[[
+-- contiguous() 这个函数的用法比较奇怪，如果Tensor的内容在memory中是连续的，就返回句柄
+-- 否则就是返回一个copy
+--]]
+x = torch.Tensor(2, 3):fill(1)
+print(x)  -- 这种情况下x中的元素在内存中是连续的，不要问我这是为什么
+y = x:contiguous():fill(2)
+print(y)  
+print(x)  -- 可以看到x就是已经改变了，因为y就是指向x的句柄
+-- 那种情况下Tensor内容不是连续的呢？ 
+z = x:t():contiguous():fill(math.pi)  -- 将t转置之后内容就不连续了 和c里面规则是一样的
+print(x)
+print(z)  -- 很明显这个就是不一样的了  这个函数到底有啥用～～～～
+
+
+--[[
+-- torch.type这个函数，很明显和lua原始的type差不多，就是为了获取数据的类型
+--]]
+print(torch.Tensor(3):fill(2.1):type())
+-- 关于类型的转化
+-- DoubleTensor  -> DoubleTensor  拷贝的是句柄
+x = torch.Tensor(5,1):fill(math.pi)
+print(x)
+y = x:type("torch.DoubleTensor")
+y:fill(0)
+print(x:t())  --可以看到改变y，就相当于直接改变了x
+
+--[[
+-- DoubleTensor ->IntTensor 拷贝的是内容, 也就是类型变化，其他的一样
+--]]
+x = torch.Tensor(5, 1):fill(math.pi)
+print(x:t())
+y = x:type("torch.IntTensor")
+print(x:t())
+print(y:t()) -- 很明显，这里改变y不再影响x，也就是说这里是内存拷贝
+
+--[[
+     isTensor()  判定某个变量是否是Tensor
+--]]
+print(torch.randn(3, 4):isTensor())  -- 很明显是Tensor
+print(torch.randn(3, 4)[1]:isTensor())
+print(torch.isTensor(torch.randn(3, 4)[1][2])) --这里是一个标量，不是Tensor
+-- 很奇怪 torch.randn(3, 4)[1][2]:isTensor()  这句话是报错的
+-- 关于类型转化还有更简单的方法
+
+x = torch.Tensor(3, 1):fill(math.pi)
+print(x:t())
+print(x:t():type("torch.IntTensor"))
+print(x:t())
+print(x:t():int())  -- 和上面的一句是等价的
+
+--[[
+Tensor的size和structure
+--]]
+x = torch.Tensor(4, 5)  -- 4 * 5
+print(x:nDimension())  -- 维度  2D
+print(x:dim()) -- 和上面的一句是等价的
+print(x:size())  -- 返回的是Tensor的结构信息
+
+print(x:size(1))  -- 获取第1维度的size
+print(x:size(2))  -- 获取第2维度的size
+
+
+
+
+
+
+
+
